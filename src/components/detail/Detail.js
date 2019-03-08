9e0cb8731bf6ed3981be00bdcfeb526411258d33
Jose Julián Bustos Díaz <jota.bustos@thekeytalent.com>
2019-03-08 18:38:18 +0100

    Detail view improved

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -27,35 +27,59 @@ class Detail extends Component {
     const { movie } = this.state;
     console.log(movie);
     const backgroundStyle = {
-      backgroundImage: `url(https://image.tmdb.org/t/p/w342${
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${
         movie.backdrop_path
       })`,
-      margin: "40px",
       backgroundRepeat: "no-repeat",
-      backgroundSize: "contain"
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "30vh",
+      width: "100%",
+      opacity: "0.8"
     };
 
     return (
-      <div className="movieitem" style={backgroundStyle}>
-        <img
-          className="movieitem__img"
-          onClick={this.seeDetail}
-          src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-          alt={movie.title}
-        />
-        <div className="movieitem__info">
-          <div className="movieitem__info__wrapper">
-            <div className="movieitem__info__wrapper_average">
-              {movie.vote_average}
+      // <div className="movieitem" style={backgroundStyle}>
+      //   <img
+      //     className="movieitem__img"
+      //     onClick={this.seeDetail}
+      //     src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+      //     alt={movie.title}
+      //   />
+      //   <div className="movieitem__info">
+      //     <div className="movieitem__info__wrapper">
+      //       <div className="movieitem__info__wrapper_average">
+      //         {movie.vote_average}
+      //       </div>
+      //       <div className="movieitem__info__wrapper_title">
+      //         {movie.title}
+      //         <div className="movieitem__info__wrapper_title_date">
+      //           {movie.release_date}
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <div className="movieitem__info__overview">{movie.overview}</div>
+      //   </div>
+      // </div>
+      <div>
+        <div className="container__background" style={backgroundStyle} />
+        <div className="container container__info">
+          <div className="row">
+            <div className="col-md-4 col-sm-12 col-xs-12 container__image">
+              <img
+                className="movieitem__img"
+                onClick={this.seeDetail}
+                src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                alt={movie.title}
+              />
             </div>
-            <div className="movieitem__info__wrapper_title">
-              {movie.title}
-              <div className="movieitem__info__wrapper_title_date">
-                {movie.release_date}
-              </div>
+            <div className="col-md-8 col-sm-12 col-xs-12 container__text">
+              <h2>{movie.title}</h2>
+              <div className="date__rank"><p>Release: {movie.release_date}</p> <p>Ranking: {movie.vote_average}</p></div>
+              <h4>Overview</h4>
+              <p>{movie.overview}</p>
             </div>
           </div>
-          <div className="movieitem__info__overview">{movie.overview}</div>
         </div>
       </div>
     );
